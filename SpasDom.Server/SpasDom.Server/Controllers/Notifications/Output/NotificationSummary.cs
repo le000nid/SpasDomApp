4740e2c1bb322fc36a50893a1a049630262fa05b
i.chiesov <i.chiesov@gmail.com>
2021-11-17 23:29:04 +0700

    server: init project.

---
+++ SpasDom.Server/SpasDom.Server/Controllers/Notifications/Output/NotificationSummary.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using SpasDom.Server.Entities;
+using System;
+
+namespace SpasDom.Server.Controllers.Notifications.Output
+{
+    public class NotificationSummary
+    {
+        public NotificationSummary(Notification source)
+        {
+            Id = source.Id;
+            Title = source.Title;
+            Body = source.Body;
+            PostedAt = source.PostedAt;
+           
+        }
+
+        [JsonProperty("id")]
+        public long Id { get; set; }
+
+        [JsonProperty("title")]
+        public string Title { get; set; }
+
+        [JsonProperty("body")]
+        public string Body { get; set; }
+
+        [JsonProperty("posted_at")]
+        public DateTimeOffset PostedAt { get; set; }
+
+        [JsonProperty("photos")]
+        public string[] Photos { get; set; }
+    }
+}
---
+++ SpasDom.Server/SpasDom.Server/Migrations/20211117161036_Initial.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SpasDom.Server.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "spas-dom");
+
+            migrationBuilder.CreateTable(
+                name: "notifications",
+                schema: "spas-dom",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Title = table.Column<string>(type: "TEXT", nullable: true),
+                    Body = table.Column<string>(type: "TEXT", nullable: true),
+                    PostedAt = table.Column<DateTimeOffset>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_notifications", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "notifications",
+                schema: "spas-dom");
+        }
+    }
+}

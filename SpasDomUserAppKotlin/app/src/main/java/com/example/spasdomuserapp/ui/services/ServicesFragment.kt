aaa3e36077ce8f3f258c04612ac85eea58e4b878
Andrew Weiss <andreivayss@gmail.com>
2021-11-23 23:49:26 +0700

    Fixed tabs

--- SpasDomUserAppKotlin/app/src/main/java/com/example/spasdomuserapp/ui/services/ServicesFragment.kt
+++ SpasDomUserAppKotlin/app/src/main/java/com/example/spasdomuserapp/ui/services/ServicesFragment.kt
@@ -6,15 +6,26 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.spasdomuserapp.R
+import com.example.spasdomuserapp.databinding.FragmentServicesBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
-class ServicesFragment : Fragment() {
+class ServicesFragment : Fragment(R.layout.fragment_services) {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_services, container, false)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val binding = FragmentServicesBinding.bind(view)
+
+        binding.apply {
+            val adapter = ViewPagerAdapter(requireActivity().supportFragmentManager, lifecycle)
+            viewPager2.adapter = adapter
+
+            TabLayoutMediator(tabLayout, viewPager2) { tab, position ->
+                when(position) {
+                    0 -> tab.text = "Экстренные"
+                    1 -> tab.text = "Плановые"
+                    2 -> tab.text = "Доп. услуги"
+                }
+            }.attach()
+        }
     }
-
 }
--- SpasDomUserAppKotlin/app/src/main/java/com/example/spasdomuserapp/ui/services/ViewPagerAdapter.kt
+++ SpasDomUserAppKotlin/app/src/main/java/com/example/spasdomuserapp/ui/services/ViewPagerAdapter.kt
@@ -1,4 +1,24 @@
 package com.example.spasdomuserapp.ui.services
 
-class ViewPagerAdapter {
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.Lifecycle
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.example.spasdomuserapp.ui.orders.EmergencyFragment
+import com.example.spasdomuserapp.ui.services.additionals.AdditionalFragment
+import com.example.spasdomuserapp.ui.services.planned.PlannedFragment
+
+class ViewPagerAdapter(fragmentManager: FragmentManager, lifecycle: Lifecycle): FragmentStateAdapter(fragmentManager, lifecycle) {
+    override fun getItemCount(): Int {
+        return 3
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        return when(position) {
+            0 -> EmergencyFragment()
+            1 -> PlannedFragment()
+            2 -> AdditionalFragment()
+            else -> Fragment()
+        }
+    }
 }

84d9d7f95e32fd89fb1ab49982eb67d18d1b2006
Andrew Weiss <andreivayss@gmail.com>
2021-11-27 17:43:52 +0700

    Implemented AuthApi.kt RemoteDataSource.kt Resource.kt. Retrofit set up

---
+++ SpasDomUserAppKotlin/app/src/main/java/com/example/spasdomuserapp/network/AuthApi.kt
@@ -0,0 +1,15 @@
+package com.example.spasdomuserapp.network
+
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.POST
+
+interface AuthApi {
+
+    @FormUrlEncoded
+    @POST("auth/login")
+    suspend fun login(
+        @Field("email") email: String,
+        @Field("password") password: String
+    ): Any
+}
---
+++ SpasDomUserAppKotlin/app/src/main/java/com/example/spasdomuserapp/network/RemoteDataSource.kt
@@ -0,0 +1,30 @@
+package com.example.spasdomuserapp.network
+
+import android.content.Context
+import androidx.viewbinding.BuildConfig
+import okhttp3.Authenticator
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class RemoteDataSource {
+
+    companion object {
+        private const val BASE_URL = "http://51.250.24.236/"
+    }
+
+
+    fun <Api> buildApi(
+        api: Class<Api>,
+       // context: Context
+    ): Api {
+        //val authenticator = TokenAuthenticator(context, buildTokenApi())
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+          //  .client(getRetrofitClient(authenticator))
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(api)
+    }
+}

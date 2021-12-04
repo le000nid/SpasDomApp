fe639f290c47f34ebd9eacf87fc55429e462b5ba
Andrew Weiss <andreivayss@gmail.com>
2021-12-04 16:48:15 +0700

    Intermediate step #6 Implemented dynamic loading of time table

--- SpasDomUserAppKotlin/app/src/main/java/com/example/spasdomuserapp/ui/services/planned/categories/date/PlannedDateFragment.kt
+++ SpasDomUserAppKotlin/app/src/main/java/com/example/spasdomuserapp/ui/services/planned/categories/date/PlannedDateFragment.kt
@@ -1,10 +1,15 @@
 package com.example.spasdomuserapp.ui.services.planned.categories.date
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.GridLayout
+import android.widget.LinearLayout
+import android.widget.RadioButton
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
@@ -14,6 +19,7 @@ import com.example.spasdomuserapp.R
 import com.example.spasdomuserapp.databinding.FragmentPlannedDateBinding
 import com.example.spasdomuserapp.models.WorkerDay
 import com.example.spasdomuserapp.ui.services.planned.categories.AddOrderViewModel
+import kotlinx.android.synthetic.main.fragment_planned_date.*
 import java.time.LocalDate
 import java.time.YearMonth
 import java.time.format.DateTimeFormatter
@@ -24,36 +30,63 @@ class PlannedDateFragment : Fragment(R.layout.fragment_planned_date) {
     private lateinit var selectedDate: LocalDate
     private var calendarViewAdapter: CalendarViewAdapter? = null
     private val viewModel: AddOrderViewModel by viewModels()
+    private lateinit var binding: FragmentPlannedDateBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val binding: FragmentPlannedDateBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_planned_date, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_planned_date, container, false)
         binding.lifecycleOwner = viewLifecycleOwner
 
         selectedDate = LocalDate.now()
 
         binding.btnLeft.setOnClickListener {
             selectedDate = selectedDate.minusMonths(1)
-            drawMonthView(binding)
+            drawMonthView()
         }
 
         binding.btnRight.setOnClickListener {
             selectedDate = selectedDate.plusMonths(1)
-            drawMonthView(binding)
+            drawMonthView()
         }
 
-        drawMonthView(binding)
+        drawMonthView()
+
 
         return binding.root
     }
 
+    @SuppressLint("ResourceType")
+    private fun drawTimeSchedule(timeList: List<String>) {
 
-    private fun drawMonthView(binding: FragmentPlannedDateBinding) {
+        val layoutParamsVar = LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+
+        binding.radioGroupTimes.clearCheck()
+        binding.radioGroupTimes.removeAllViews()
+
+        for (i in timeList.indices) {
+            val radioButton = RadioButton(requireContext())
+            radioButton.layoutParams = layoutParamsVar
+            radioButton.text = timeList[i]
+            radioButton.id = i
+            binding.radioGroupTimes.addView(radioButton)
+        }
+
+        radioGroupTimes.setOnCheckedChangeListener { group, checkedId ->
+
+        }
+    }
+
+    private fun drawMonthView() {
         calendarViewAdapter = CalendarViewAdapter(DateClick {
-            Log.i("click", it.toString())
+            if (it.timesList != null) {
+                drawTimeSchedule(it.timesList)
+            } else {
+                //TODO(make snackbar)
+            }
+
         })
 
         binding.root.findViewById<RecyclerView>(R.id.calendarRecyclerView).apply {

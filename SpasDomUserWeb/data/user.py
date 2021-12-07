a34e4f8ab6de0d201e0b5669f2547d64b2afb079
sikren <57663671+sikren@users.noreply.github.com>
2021-12-07 19:25:01 +0300

    Commit to show tomorrow (8.12.21)
    (House form + Apartment's on Announc. page) were added

--- SpasDomUserWeb/data/user.py
+++ SpasDomUserWeb/data/user.py
@@ -1,7 +1,6 @@
 import sqlalchemy
 from .db_session import SqlAlchemyBase
 from flask_login import UserMixin
-from werkzeug.security import generate_password_hash, check_password_hash
 
 
 class User(SqlAlchemyBase, UserMixin):
@@ -10,15 +9,9 @@ class User(SqlAlchemyBase, UserMixin):
     id = sqlalchemy.Column(sqlalchemy.Integer,
                            primary_key=True, autoincrement=True)
     username = sqlalchemy.Column(sqlalchemy.String, nullable=False)
-    email = sqlalchemy.Column(sqlalchemy.String,
-                              index=True, unique=True, nullable=True)
-    about = sqlalchemy.Column(sqlalchemy.String, nullable=True)
-    photo = sqlalchemy.Column(sqlalchemy.Binary, nullable=True)
 
-    hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    access_token = sqlalchemy.Column(sqlalchemy.String, nullable=False)
+    access_expiry = sqlalchemy.Column(sqlalchemy.REAL, nullable=False)
 
-    def set_password(self, password):
-        self.hashed_password = generate_password_hash(password)
-
-    def check_password(self, password):
-        return check_password_hash(self.hashed_password, password)
+    refresh_token = sqlalchemy.Column(sqlalchemy.String, nullable=False)
+    refresh_expiry = sqlalchemy.Column(sqlalchemy.REAL, nullable=False)

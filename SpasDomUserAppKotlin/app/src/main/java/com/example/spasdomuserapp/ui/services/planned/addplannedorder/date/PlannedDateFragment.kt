deffea12f215852e3b517dcf1bac3b7e8c5285a1
Andrew Weiss <andreivayss@gmail.com>
2021-12-14 22:01:26 +0700

    Implemented template for market calendar

--- SpasDomUserAppKotlin/app/src/main/java/com/example/spasdomuserapp/ui/services/planned/addplannedorder/date/PlannedDateFragment.kt
+++ SpasDomUserAppKotlin/app/src/main/java/com/example/spasdomuserapp/ui/services/planned/addplannedorder/date/PlannedDateFragment.kt
@@ -78,12 +78,17 @@ class PlannedDateFragment : Fragment(R.layout.fragment_planned_date) {
             }
 
             val date = viewModel.date + " " + viewModel.time
-            var finalOrder = args.plannedOrderPost
-            finalOrder = finalOrder?.copy(date = date, workerId = viewModel.workerId!!)
-            val cacheOrder = Order(4, "Проверка воды", viewModel.date.toString(), viewModel.time.toString(), 0, "",0, "", "Алексей Воронов", 5, "Опытный специалист")
 
-            //viewModel.postPlannedOrder(finalOrder)
-            viewModel.insertPlannedOrderToCache(cacheOrder)
+            if (args.plannedOrderPost != null) {
+                var finalOrder = args.plannedOrderPost
+                finalOrder = finalOrder?.copy(date = date, workerId = viewModel.workerId!!)
+                val cacheOrder = Order(4, "Проверка воды", viewModel.date.toString(), viewModel.time.toString(), 0, "",0, "", "Алексей Воронов", 5, "Опытный специалист")
+                //viewModel.postPlannedOrder(finalOrder)
+                viewModel.insertPlannedOrderToCache(cacheOrder)
+            } else if (args.workerPreview != null) {
+
+            }
+
             val action = PlannedDateFragmentDirections.actionPlannedDateFragmentToSuccessFragment()
             findNavController().navigate(action)
         }

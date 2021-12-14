95c452f23e8c2b822ef5171e27f3643960d3536f
Andrew Weiss <andreivayss@gmail.com>
2021-12-14 19:44:53 +0700

    Added data to the MarketWorkersViewModel.kt

--- SpasDomUserAppKotlin/app/src/main/java/com/example/spasdomuserapp/ui/services/market/workers/MarketWorkersViewModel.kt
+++ SpasDomUserAppKotlin/app/src/main/java/com/example/spasdomuserapp/ui/services/market/workers/MarketWorkersViewModel.kt
@@ -1,4 +1,38 @@
 package com.example.spasdomuserapp.ui.services.market.workers
 
-class MarketWorkersViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.spasdomuserapp.models.WorkerPreview
+import com.example.spasdomuserapp.network.Resource
+import com.example.spasdomuserapp.repository.ServicesRepository
+import com.example.spasdomuserapp.responses.WorkersPreviewResponse
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class MarketWorkersViewModel @Inject constructor(
+    private val repository: ServicesRepository
+) : ViewModel() {
+
+    private val _marketPreviewWorkers: MutableLiveData<Resource<WorkersPreviewResponse>> = MutableLiveData()
+    val marketPreviewWorkers: LiveData<Resource<WorkersPreviewResponse>>
+        get() = _marketPreviewWorkers
+
+    fun getPreviewWorkers() = viewModelScope.launch {
+        _marketPreviewWorkers.value = Resource.Loading
+        _marketPreviewWorkers.value = repository.getMarketPreviewWorkers()
+    }
+
+    val previewWorkers: List<WorkerPreview> = listOf(
+        WorkerPreview(1, "Василий", "Петров", "", "2000", 4, "2 года"),
+        WorkerPreview(2, "Александр", "Васильев", "", "1400", 3, "1.5 года"),
+        WorkerPreview(3, "Виталий", "Сачков", "", "1800", 4, "3 года"),
+        WorkerPreview(4, "Алексей", "Мухов", "", "3000", 5, "2.5 года"),
+        WorkerPreview(5, "Светлана", "Баламова", "", "1500", 3, "1 год"),
+        WorkerPreview(6, "Михаил", "Абрамова", "", "800", 2, "2 месяца"),
+    )
+
 }
